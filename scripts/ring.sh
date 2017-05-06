fa248450a923169d9dd854f9a6ffba9a6c382976
Scarlett Clark <sgclark@kde.org>
2017-05-06 05:15:00 -0700

    Fix cflags and kdflags

--- scripts/ring.sh
+++ scripts/ring.sh
@@ -4,7 +4,8 @@ set -x
 export LD_LIBRARY_PATH=/opt/usr/lib:/opt/usr/library:/opt/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib64
 export PKG_CONFIG="pkg-config --static"
 export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
-export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
+export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $(shell pkg-config --cflags argon2) $LDFLAGS"
+export CFLAGS="$(shell pkg-config --libs argon2) $CXXFLAGS"
 cd /source/
 rm -rfv ring-daemon
 git clone https://github.com/savoirfairelinux/ring-daemon
